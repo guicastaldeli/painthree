1d4f6519bbeb612be839cd521c11c836baf9d7c2
gui castaldeli <guicastaldeli8@gmail.com>
2026-08-16 20:22:54 -0400

    Update

--- scene.ts
+++ scene.ts
@@ -11,16 +11,45 @@ import { vec3 } from "gl-matrix";
  */
 let camera: data.Camera | null = null;
 
-const POS_X = 0.0;
-const POS_Y = 0.0;
-const POS_Z = 0.0;
+const CAMERA_POS_X = 0.0;
+const CAMERA_POS_Y = 0.0;
+const CAMERA_POS_Z = 0.0;
 
 // Set Camera
 function setCamera(): void {
     if(!camera) {
-        camera = data.setCamera([POS_X, POS_Y, POS_Z]);
+        camera = data.setCamera([CAMERA_POS_X, CAMERA_POS_Y, CAMERA_POS_Z]);
         console.log('Camera initialized');
     }
+}
+
+/**
+ * 
+ * Skybox
+ * 
+ */
+const SKYBOX_POS_X = 0.0;
+const SKYBOX_POS_Y = 0.0;
+const SKYBOX_POS_Z = 0.0;
+
+const SKYBOX_SCALE_X = 100.0;
+const SKYBOX_SCALE_Y = 100.0;
+const SKYBOX_SCALE_Z = 100.0;
+
+const SKYBOX_COLOR = 1.0;
+
+// Set Skybox
+function setSkybox(): void {
+    const id = 'skybox';
+
+    data.addMesh(id, data.MeshType.CUBE, 
+        vec3.fromValues(SKYBOX_POS_X, SKYBOX_POS_Y, SKYBOX_POS_Z), 
+        vec3.fromValues(SKYBOX_COLOR, SKYBOX_COLOR, SKYBOX_COLOR));
+    data.setMeshScale(id, 
+        SKYBOX_SCALE_X, 
+        SKYBOX_SCALE_Y, 
+        SKYBOX_SCALE_Z
+    )
 }
 
 /**
@@ -34,8 +63,10 @@ export function render(): void {
     setCamera();
     data.updateCamera();
 
-    data.updateCollision(data.getCameraPosition());
+    //data.updateCollision(data.getCameraPosition());
     
+    setSkybox();
+
     angle += 0.01;
 
     data.addMesh('cube', data.MeshType.CUBE, vec3.fromValues(0.8, 0.5, 0), vec3.fromValues(0.2, 0.8, 1.0));
